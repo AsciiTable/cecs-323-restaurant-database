a004c5181965e416effcf6dc5fae091ac4a0bb49
Jessica Wei <jessicawei9@gmail.com>
2019-12-08 02:56:32 -0800

    taking note of what's required for views

---
+++ create_view.sql
@@ -0,0 +1,20 @@
+/* View DDL */
+/*MenuItem_v – For each menu item, give it’s spiciness, and all of the different costs for
+that item. If a given item is not on a particular menu, then report “N/A” for that particular
+item for that particular menu. Also, if an item only appears as a single serving portion,
+put in “N/A” into the report for the gallon, … prices.*/
+
+
+/*Customer_addresses_v – for each customer, indicate whether they are an individual or a
+corporate account, and display all of the information that we are managing for that
+customer.*/
+
+/*Sous_mentor_v – reports all the mentor/mentee relationships at Miming’s, sorted by the
+name of the mentor, then the name of the mentee. Show the skill that the mentorship
+passes, as well as the start date.*/
+
+/*Customer_Sales_v – On a year by year basis, show how much each customer has spent at
+Miming’s.*/
+
+/*Customer_Value_v – List each customer and the total $ amount of their orders for the
+past year, in order of the value of customer orders, from highest to the lowest.*/
