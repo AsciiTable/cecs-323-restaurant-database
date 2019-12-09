7ffb6b0b44c1875502b97d1d53af52e89f64999c
Jessica Wei <jessicawei9@gmail.com>
2019-12-09 13:37:32 -0800

    added distinct to spiciness

--- create_view.sql
+++ create_view.sql
@@ -4,7 +4,7 @@ that item. If a given item is not on a particular menu, then report “N/A” fo
 item for that particular menu. Also, if an item only appears as a single serving portion,
 put in “N/A” into the report for the gallon, … prices.*/
 /*CREATE VIEW MenuItem_v AS*/
-SELECT menuType AS Menu, foodItemName AS Item, GROUP_CONCAT(spiceLevel) AS Spiciness, size AS Size, price AS Price
+SELECT menuType AS Menu, foodItemName AS Item, GROUP_CONCAT(DISTINCT spiceLevel) AS Spiciness, size AS Size, price AS Price
 FROM MenuItem
 GROUP BY foodItemName;
 
